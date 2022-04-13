f66121ad818f681ab0d80c07a003d35d6e870bf1
claudioiturburu <claudioiturburu@gmail.com>
2022-04-13 16:19:28 -0300

    commit tp 3

--- src/Estudiante.js
+++ src/Estudiante.js
@@ -1,19 +1,69 @@
 import React, { Component } from 'react'
 import "./Estudiante.css"
 
-export default class Estudiante extends Component {
-  render() {
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-    let estudiante = {
-        nombre: "Claudio",
-        apellido: "Iturburu",
+function getRandomString(min, max) {
+    let texto = '';
+    const caracteres = "abcdefghijklmnñopqrstuvwxyz";
+    let size = getRandomInt(min,max);
+    for (let index = 0; index < size-1; index++) {
+        let posicion = getRandomInt(1,27) - 1;
+        texto+= caracteres.substring(posicion, posicion+1);
+    }
+    return texto;
+}
+
+export default class Estudiante extends Component {
+
+    constructor(props) {
+        let descripcion = getRandomString(5,10);
+        let horas = getRandomInt(0,300);
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            cursos: [{descripcion: descripcion,horas: horas}],
+        };
     }
 
-    return (
-      <div>
-        <p className="card">
-        {estudiante.nombre + " " + estudiante.apellido}</p>
-      </div>
-    )
-  }
+    handleClick() {
+        this.setState((state) => ({
+            cursos: [...state.cursos, {descripcion: getRandomString(5,10),horas: getRandomInt(0,300)}],
+        }));
+    }
+
+    render() {
+        return (
+            <>
+                <div>
+                    <p className="card">
+                        {this.props.estudiante.nombre + " " + this.props.estudiante.apellido}
+                    </p>
+                    <p>
+                        <button className="btn-center" onClick={this.handleClick}>Inscribirme</button>
+                    </p>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Curso</th>
+                                <th>Cantidad de horas</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.cursos.map(
+                                (cursos,index) => (
+                                    <tr key={index}>
+                                        <td>{cursos.descripcion}</td>
+                                        <td>{cursos.horas} horas semanales</td>
+                                    </tr>
+                                )
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </>
+        )
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Estudiante from './Estudiante';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let estudiante = {
+  nombre: "Claudio",
+  apellido: "Iturburu",
+}
+
 root.render(
   <React.StrictMode>
-    <Estudiante />
-    <Estudiante />
+    <Estudiante estudiante={estudiante} />
   </React.StrictMode>
 );
 

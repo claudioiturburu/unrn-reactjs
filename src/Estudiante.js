47d023b0ffa47b3dcbd1fd40e6fd39b2d491ae59
claudioiturburu <claudioiturburu@gmail.com>
2022-04-19 21:29:58 -0300

    commit tp4

--- src/Estudiante.js
+++ src/Estudiante.js
@@ -1,52 +1,54 @@
 import React, { Component } from 'react'
 import "./Estudiante.css"
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function getRandomString(min, max) {
-    let texto = '';
-    const caracteres = "abcdefghijklmnñopqrstuvwxyz";
-    let size = getRandomInt(min,max);
-    for (let index = 0; index < size-1; index++) {
-        let posicion = getRandomInt(1,27) - 1;
-        texto+= caracteres.substring(posicion, posicion+1);
-    }
-    return texto;
-}
-
 export default class Estudiante extends Component {
 
     constructor(props) {
-        let descripcion = getRandomString(5,10);
-        let horas = getRandomInt(0,300);
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+        this.listarCursos = this.listarCursos.bind(this);
+        this.listarEstudiante = this.listarEstudiante.bind(this);
         this.state = {
-            cursos: [{descripcion: descripcion,horas: horas}],
+            cursos: [],
+            estudiantes: [],
         };
     }
 
-    handleClick() {
-        this.setState((state) => ({
-            cursos: [...state.cursos, {descripcion: getRandomString(5,10),horas: getRandomInt(0,300)}],
-        }));
+    listarCursos() {
+        const url = 'http://localhost:1234/cursos';
+        fetch(url)
+            .then((respuesta) => respuesta.json())
+            .then((json) => (
+                this.setState({
+                    resultado: json.success,
+                    cursos: json.cursos,
+                })
+            ));
+    }
+
+    listarEstudiante(idEstudiante) {
+        const url = 'http://localhost:1234/estudiantes?idEstudiante='+idEstudiante;
+        fetch(url)
+            .then((respuesta) => respuesta.json())
+            .then((json) => (
+                this.setState({
+                    resultado: json.success,
+                    estudiantes: json.estudiantes,
+                })
+            ))
     }
 
     render() {
         return (
             <>
                 <div>
-                    <p className="card">
-                        {this.props.estudiante.nombre + " " + this.props.estudiante.apellido}
-                    </p>
                     <p>
-                        <button className="btn-center" onClick={this.handleClick}>Inscribirme</button>
+                        <button className="btn-center" onClick={this.listarCursos}>Listar Cursos</button>
+                        <button className="btn-center" onClick={this.listarEstudiante("2")}>Listar Estudiante 2</button>
                     </p>
                     <table className="table">
                         <thead>
                             <tr>
+                                <th>Id Curso</th>
                                 <th>Curso</th>
                                 <th>Cantidad de horas</th>
                             </tr>
@@ -55,8 +57,39 @@ export default class Estudiante extends Component {
                             {this.state.cursos.map(
                                 (cursos,index) => (
                                     <tr key={index}>
+                                        <td>{cursos.idCurso}</td>
                                         <td>{cursos.descripcion}</td>
                                         <td>{cursos.horas} horas semanales</td>
+                                    </tr>
+                                )
+                            )}
+                        </tbody>
+                    </table>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Id Estudiante</th>
+                                <th>Apellido</th>
+                                <th>Nombre</th>
+                                <th>Cursos</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.estudiantes.map(
+                                (estudiantes,index) => (
+                                    <tr key={index}>
+                                        <td>{estudiantes.idEstudiante}</td>
+                                        <td>{estudiantes.apellido}</td>
+                                        <td>{estudiantes.nombre}</td>
+                                        <td>
+                                            <ul>
+                                                {estudiantes.cursos && estudiantes.cursos.map(
+                                                    (cursos,index) => (
+                                                        <li>{cursos.descripcion + " " + cursos.horas + " horas semanales"}</li>
+                                                    )
+                                                )}
+                                            </ul>
+                                        </td>
                                     </tr>
                                 )
                             )}
